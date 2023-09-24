28cefe17143c3e5951dfdd24c9cf65cec34a9e8f
juliawgraham <juliagra@andrew.cmu.edu>
2023-09-24 17:16:55 -0400

    added str

--- Connect4/backend/Checker.py
+++ Connect4/backend/Checker.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
 class Checker(Enum):
+    def __str__(self):
+        return str(self.value)
+    
     EMPTY = ' '
     YELLOW = 'Y'
     RED = 'R'
--- Connect4/backend/GameState.py
+++ Connect4/backend/GameState.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
 class GameState(Enum):
+    def __str__(self):
+        return str(self.value)
+    
     TURN_PLAYER_1 = 'player 1\'s turn'
     TURN_PLAYER_2 = 'player 2\'s turn'
     TIE = 'the game has ended in a tie'
