eb34e1c2a356c1838de1b913989a6f94a2a614ce
juliawgraham <juliagra@andrew.cmu.edu>
2023-09-24 16:18:39 -0400

    added separate game states for player's winning

--- Connect4/backend/GameState.py
+++ Connect4/backend/GameState.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class GameState(Enum):
-    IN_PROGRESS = 'in progress'
-    TIE = 'tie'
-    WIN = 'win'
+    IN_PROGRESS = 'the game is in progress'
+    TIE = 'the game has ended in a tie'
+    WIN_PLAYER_1 = 'player 1 win\'s'
+    WIN_PLAYER_2 = 'player 2 win\'s'
