a9aa18f6db1ccffdbc0d11ae8fb816b157fbed53
juliawgraham <juliagra@andrew.cmu.edu>
2023-09-24 17:01:47 -0400

    bug - updated col to col_index where needed

--- Connect4/backend/Connect4.py
+++ Connect4/backend/Connect4.py
@@ -36,8 +36,12 @@ class Connect4:
             self._current_player = 1
             self._game_state = GameState.TURN_PLAYER_1
     
-    def __is_game_in_progress():
+    def __is_game_in_progress(self):
         return GameState.TURN_PLAYER_1 or GameState.TURN_PLAYER_2
+    
+    # ????
+    def game_state(self):
+        return self._game_state
     
     def board(self):
         return self._board
@@ -47,12 +51,13 @@ class Connect4:
             self._board.append([Checker.EMPTY] * self.BOARD_WIDTH)
 
     def is_valid_move(self, col):
+        col_index = col - 1
         try:
             if not self.__is_game_in_progress():
                 raise GameOverError
             elif (col < 1 or col > self.BOARD_WIDTH):
                 raise ValueError
-            elif not any([row[col] is Checker.EMPTY for row in self._board]):
+            elif not any([row[col_index] is Checker.EMPTY for row in self._board]):
                 raise ColumnFullError
             return True
         
@@ -69,45 +74,49 @@ class Connect4:
             game._game_state = GameState.TIE
             return
         
-        # check horizontal wins
+        # check horizontal win
         # self.BOARD_WIDTH - self.WIN_LENGTH + 1 = 7 - 4 + 1 = 4 --> [0-3]
         for i in range(self.BOARD_WIDTH - self.WIN_LENGTH + 1):
-            if all([checker is self.__current_player_checker for checker in self._board[i, i+self.WIN_LENGTH]]):
+            if all([checker is self.__current_player_checker() for checker in self._board[i, i+self.WIN_LENGTH]]):
                 game._game_state = self.__current_player_win_game_state()
                 return
         
-        # check vertical wins
+        # check vertical win
+        # for i in range(self.BOARD_HEIGHT - self.WIN_LENGTH + 1):
+        #     if all([True for row in self._board if row[col_index] is self.__current_player_checker()]):
+        #         game._game_state = self.__current_player_win_game_state()
+        #         return
+            
+        # check positive diagonal win
 
-        # check positive diagonal wins
-
-        # check negative diagonal wins
+        # check negative diagonal win
 
     def move(self, col):
+        col_index = col - 1
         # check if the move is valid
         # if the move is not valid, an error is thrown
         game.is_valid_move(col)
 
         # find the row where the checker will drop to
-        row_to_drop = max(row_index for row_index, row in enumerate(self._board) if row[col] is Checker.EMPTY)
+        row_index = max(row_index for row_index, row in enumerate(self._board) if row[col_index] is Checker.EMPTY)
         
         # update the board with the new piece
-        self._board[row_to_drop][col] = self.__current_player_checker()
+        self._board[row_index][col_index] = self.__current_player_checker()
         
         # update the game state based on the move made
-        self.__update_game_state(row_to_drop, col)
+        self.__update_game_state(row_index, col_index)
 
         # if the game is in progress, switch to the next player
         if self.__is_game_in_progress():
             self.__switch_current_player()
-
+        
         return self._game_state
-
-
 
 def make_move(game, col):
     print(f"current player: {game.current_player()}")
-    game.move(col)
+    game_state = game.move(col)
     print('\n'.join([' | '.join([checker.value for checker in row]) for row in game.board()]))
+    print(game_state.value)
     print()
 
 game = Connect4()
