d57b714fe7fb18cbbe7396bd991f5c1aa0b0af33
Elvis Iraguha <iraguhaelvis@gmail.com>
2023-09-29 21:38:26 -0400

    remove comments

--- CLI.java
+++ CLI.java
@@ -11,29 +11,24 @@ import connect4.Player;
 import connect4.Game.Status;
 
 public class CLI {
-    
+
     private void drawBoard(Optional<Player>[][] board) {
-        // this is definitely not the prettiest code out there
-        // feel free to rewrite this if you want
-        
+
         for (Optional<Player>[] row : board) {
             for (int col_index = 0; col_index < row.length; col_index++) {
                 if (col_index == 0) {
                     System.out.print("|_ ");
-                }
-                else {
+                } else {
                     System.out.print(" _|_ ");
                 }
-                     
+
                 if (row[col_index].isPresent()) {
                     if (row[col_index].get() == Player.RED) {
                         System.out.print("R");
-                    }
-                    else if (row[col_index].get() == Player.YELLOW) {
+                    } else if (row[col_index].get() == Player.YELLOW) {
                         System.out.print("Y");
                     }
-                }
-                else {
+                } else {
                     System.out.print(" ");
                 }
             }
@@ -45,28 +40,22 @@ public class CLI {
         if (status instanceof Status.Turn turnStatus) {
             if (turnStatus.player() == Player.RED) {
                 System.out.println("Red's turn");
-            }
-            else if (turnStatus.player() == Player.YELLOW) {
+            } else if (turnStatus.player() == Player.YELLOW) {
                 System.out.println("Yellow's turn");
             }
         }
-
-        // status can also indicate whether the game has ended
-        // code here for printing red win/yellow win/tie
     }
 
     private Player getPlayerColor() {
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println("Would you like to play as red or yellow? (r/y)");
             String color = scanner.nextLine();
             if (color.equals("r")) {
                 return Player.RED;
-            }
-            else if (color.equals("y")) {
+            } else if (color.equals("y")) {
                 return Player.YELLOW;
-            }
-            else {
+            } else {
                 System.out.println("Invalid input. Please try again.");
             }
         }
@@ -77,10 +66,10 @@ public class CLI {
         // pick a random one
         // return it
 
-        Set<Integer> moves = game.getPossibleMoves();
+        Set<Integer> possibleMoves = game.getPossibleMoves();
 
         // Convert the Set to a List
-        List<Integer> movesList = new ArrayList<>(moves);
+        List<Integer> movesList = new ArrayList<>(possibleMoves);
 
         // Generate a random index within the bounds of the List
         Random random = new Random();
@@ -96,39 +85,32 @@ public class CLI {
         // check if it's a valid move
         // return it
 
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.println("Enter a column to play in [0 - 6]: ");
-            int col = scanner.nextInt();
-            if (col >= 0 && col <= 6) {
-                return col;
-            }
-            else {
+        Set<Integer> possibleMoves = game.getPossibleMoves();
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a column to play valid columns are " + possibleMoves);
+            int col;
+            try {
+                col = scanner.nextInt();
+
+                if (!possibleMoves.contains(col)) {
+                    System.out.println("Illegal move. Please try again.");
+                } else {
+                    return col;
+                }
+            } catch (Exception e) {
                 System.out.println("Invalid input. Please try again.");
+                continue;
             }
+
         }
     }
 
-
-    /* Assignment:
-    Write code that uses that API, producing a simple text-based client 
-    that allows one human player to play against an automatic computer opponent. 
-    The computer opponent does not need to be good at the game; 
-    it may, for instance, randomly play a checker in a valid column at each turn.
-    */
     public static void main(String args[]) {
-    
+
         CLI cli = new CLI();
 
-        // while loop probably somewhere here like how you did it in Python
-        // honestly, probably very similar code to what we did for our API
-        // except that for this assignment, we will have a computer 
-        // who will use .getPossibleMoves() and then make a random valid move
-
-        // the documentation is kind of a lot but in essence, they have a 
-        // very similar design to us
-
-       
         Player playerColor = cli.getPlayerColor();
 
         Game game = new Game(playerColor);
@@ -137,19 +119,20 @@ public class CLI {
             cli.drawBoard(game.getBoard());
             cli.printStatus(game.getStatus());
 
+            System.out.println(game.getPossibleMoves());
+
             int col;
 
-            if(game.getTurn().get() == playerColor) {
+            if (game.getTurn().get() == playerColor) {
                 col = cli.getPlayerMove(game);
-            }
-            else {
+            } else {
                 col = cli.getComputerMove(game);
             }
-            
+
             game.move(game.getTurn().get(), col);
 
         }
-        System.out.println(game.getStatus());
+        System.out.println(game.getGameResult().get());
 
         System.out.println("Game over!");
         System.out.println("Final board:");
@@ -158,23 +141,3 @@ public class CLI {
 
     }
 }
-
-
-/*
-Notes
-- why does move take in the player? Perhaps it would be more connivent if move 
-just took in col and made the move for the current player automatically? 
-An exception is thrown if the player is moving out of turn so I don't see why
-this needs to be a parameter as you would always just put in the current player.
-
-- is it worth for move to be more specific about why an IllegalStateException 
-was thrown? So specifying that the the column is full, the game is over, etc.?
-Possibly through the use of custom errors? Not sure if the user really needs this info tho.
-
-- perhaps be more clear in the documentation what a board is comprised of.
-the return type implies it is a 2d array of Players and getSquare() mentions that
-an empty cell is Optional.empty(), but it might be clearer if this information
-was explicit within getBoard()'s definition
-
-*/
-
